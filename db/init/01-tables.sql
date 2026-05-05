bde00499cd570781b7b58e3ce939a35d62e4cf7d
rnegash <rnegash@gmail.com>
2026-05-05 10:24:06 +0200

    populate db with elements

--- db/init/01-tables.sql
+++ db/init/01-tables.sql
@@ -1,10 +1,29 @@
-CREATE TABLE IF NOT EXISTS users (
+CREATE TABLE IF NOT EXISTS blocks (
     id SERIAL PRIMARY KEY,
-    username VARCHAR(255) NOT NULL UNIQUE
+    name VARCHAR(1) NOT NULL UNIQUE
 );
 
-CREATE TABLE IF NOT EXISTS sessions (
+CREATE TABLE IF NOT EXISTS periods (
     id SERIAL PRIMARY KEY,
-    username VARCHAR(255) NOT NULL,
-    session_id VARCHAR(255) NOT NULL UNIQUE
+    number INT NOT NULL UNIQUE CHECK (number BETWEEN 1 AND 7)
+);
+
+CREATE TABLE IF NOT EXISTS groups (
+    id SERIAL PRIMARY KEY,
+    number INT NOT NULL UNIQUE CHECK (number BETWEEN 1 AND 18),
+    name VARCHAR(50)
+);
+
+CREATE TABLE IF NOT EXISTS elements (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(50) NOT NULL UNIQUE,
+    symbol VARCHAR(3) NOT NULL UNIQUE,
+    atomic_number INT NOT NULL UNIQUE,
+    atomic_weight DECIMAL(6, 3),
+    group_id INT NOT NULL,
+    period_id INT NOT NULL,
+    block_id INT NOT NULL,
+    FOREIGN KEY (group_id) REFERENCES groups(id),
+    FOREIGN KEY (period_id) REFERENCES periods(id),
+    FOREIGN KEY (block_id) REFERENCES blocks(id)
 );
